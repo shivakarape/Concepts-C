8e923a0d4cb8b86278735752c234a5d1145480db
Shivanand Aabasaheb Karape <132795008+shivakarape@users.noreply.github.com>
2023-06-18 18:16:04 +0530

    Add files via upload

---
+++ structure7.c
@@ -0,0 +1,22 @@
+//nested structure
+#include<stdio.h>
+
+struct Demo
+{
+    int i;
+    float f;
+};
+
+struct Hello
+{
+    int no;
+    int x;
+    struct Demo dobj;
+};
+
+int main()
+{
+    struct Hello hobj;
+
+    return 0;
+}
